e7b5240d6bad8da3713cb171d22a92f2bf6178f0
Tyler Young <tyler@x-plane.com>
2017-10-09 16:43:36 -0500

    fix (xplaneCheckout): Fix for failures on Mac & Windows

--- vars/xplaneCheckout.groovy
+++ vars/xplaneCheckout.groovy
@@ -1,24 +1,38 @@
 def call(String branchName='', String checkoutDir='', String platform='') {
     dir(checkoutDir) {
         try {
-            sshagent(['tylers-ssh']) {
-                echo "Checking out ${branchName} on ${platform}"
-                sh "git branch"
-                sh "git fetch"
-                sh "git checkout ${branchName}"
-                try {
-                    sh "git pull"
-                } catch(e) { } // If we're in detached HEAD mode, pull will fail
-
-                String commitId = ""
-                if(isUnix()) {
-                    commitId = sh(returnStdout: true, script: "git rev-parse HEAD").trim()
-                } else {
-                    commitId = bat(returnStdout: true, script: "git rev-parse HEAD").trim().split("\r?\n")[1]
+            echo "Checking out ${branchName} on ${platform}"
+            if(platform == 'Linux') {
+                sshagent(['tylers-ssh']) {
+                    sh "git branch"
+                    sh "git fetch"
+                    sh "git checkout ${branchName}"
+                    try {
+                        sh "git pull"
+                    } catch(e) { } // If we're in detached HEAD mode, pull will fail
                 }
-                echo "Checked out commit ${commitId}"
+            } else {
+                checkout(
+                        [$class: 'GitSCM', branches: [[name: branchName]],
+                         doGenerateSubmoduleConfigurations: false,
+                         extensions: [
+                                 [$class: 'BuildChooserSetting', buildChooser: [$class: 'AncestryBuildChooser', ancestorCommitSha1: '', maximumAgeInDays: 120]]
+                         ],
+                         submoduleCfg: [],
+                         userRemoteConfigs:  [[credentialsId: 'tylers-ssh', url: 'ssh://tyler@dev.x-plane.com/admin/git-xplane/design.git']]]
+                )
+            }
 
-                if(isUnix()) {
+            String commitId = ""
+            if(isUnix()) {
+                commitId = sh(returnStdout: true, script: "git rev-parse HEAD").trim()
+            } else {
+                commitId = bat(returnStdout: true, script: "git rev-parse HEAD").trim().split("\r?\n")[1]
+            }
+            echo "Checked out commit ${commitId}"
+
+            if(isUnix()) {
+                sshagent(['tylers-ssh']) {
                     echo "Pulling SVN art assets too for later auto-testing"
                     // Do recursive cleanup, just in case
                     sh(returnStdout: true, script: "set +x find Aircraft  -type d -exec \"svn cleanup\" \\;")
