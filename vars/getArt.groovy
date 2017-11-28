5ea292e50817f007fb63e2c3c69954156e8e061a
Tyler Young <tyler@x-plane.com>
2017-11-27 23:17:27 -0600

    fix (tester): Fix for getArt step

--- vars/getArt.groovy
+++ vars/getArt.groovy
@@ -1,12 +1,11 @@
 def call() {
-        if(isUnix()) {
-            sshagent(['tylers-ssh']) {
-                echo "Pulling SVN art assets too for later auto-testing"
-                // Do recursive cleanup, just in case
-                sh(returnStdout: true, script: "set +x find Aircraft  -type d -exec \"svn cleanup\" \\;")
-                sh(returnStdout: true, script: "set +x find Resources -type d -exec \"svn cleanup\" \\;")
-                sh(returnStdout: true, script: 'scripts/get_art.sh checkout tyler')
-            }
+    if(isUnix()) {
+        sshagent(['tylers-ssh']) {
+            echo "Pulling SVN art assets too for later auto-testing"
+            // Do recursive cleanup, just in case
+            sh(returnStdout: true, script: "set +x find Aircraft  -type d -exec \"svn cleanup\" \\;")
+            sh(returnStdout: true, script: "set +x find Resources -type d -exec \"svn cleanup\" \\;")
+            sh(returnStdout: true, script: 'scripts/get_art.sh checkout tyler')
         }
     }
 }
