a838569a488f2d1a3c7a7e7115e0c201b37f0d7f
Tyler Young <tyler@x-plane.com>
2017-11-27 23:04:46 -0600

    fix (test): Fix for broken initialization

--- src/tester.groovy
+++ src/tester.groovy
@@ -25,11 +25,13 @@ isRenderingRegression = isRenderingRegressionMaster || isRenderingRegressionComp
 expectedScreenshotNames = isSmokeTest ? ["sunset_scattered_clouds", "evening", "stormy"] : []
 String nodeType = platform == 'Windows' ? 'windows' : (platform == 'Linux' ? 'linux' : 'mac')
 node(nodeType) {
-    checkoutDir = utils.getCheckoutDir(platform)
-    renderingRegressionMaster = utils.getArchiveRoot(platform) + 'rendering-master/'
-    archiveDir = isRenderingRegressionMaster ?
-            renderingRegressionMaster :
-            utils.getArchiveDir(platform) + (isRenderingRegressionComparison ? 'rendering-regression/' : '')
+    dir(checkoutDir) {
+        checkoutDir = utils.getCheckoutDir(platform)
+        renderingRegressionMaster = utils.getArchiveRoot(platform) + 'rendering-master/'
+        archiveDir = isRenderingRegressionMaster ?
+                renderingRegressionMaster :
+                utils.getArchiveDir(platform) + (isRenderingRegressionComparison ? 'rendering-regression/' : '')
+    }
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------
