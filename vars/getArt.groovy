431671fe1654383c786358ab3b5c621899df8533
Tyler Young <tyler@x-plane.com>
2018-05-03 10:43:01 -0500

    fix (getArt): If you're calling the getArt() step on Windows, you better have sh set up to run the get_art.sh script

--- vars/getArt.groovy
+++ vars/getArt.groovy
@@ -1,13 +1,11 @@
 def call(String checkoutDir) {
-    if(isUnix()) {
-        dir(checkoutDir) {
-            sshagent(['tylers-ssh']) {
-                echo "Pulling SVN art assets too for later auto-testing"
-                // Do recursive cleanup, just in case
-                sh(returnStdout: true, script: "set +x find Aircraft  -type d -exec \"svn cleanup\" \\;")
-                sh(returnStdout: true, script: "set +x find Resources -type d -exec \"svn cleanup\" \\;")
-                sh(returnStdout: true, script: 'scripts/get_art.sh --clobber checkout tyler')
-            }
+    dir(checkoutDir) {
+        sshagent(['tylers-ssh']) {
+            echo "Pulling SVN art assets too for later auto-testing"
+            // Do recursive cleanup, just in case
+            sh(returnStdout: true, script: "set +x find Aircraft  -type d -exec \"svn cleanup\" \\;")
+            sh(returnStdout: true, script: "set +x find Resources -type d -exec \"svn cleanup\" \\;")
+            sh(returnStdout: true, script: 'scripts/get_art.sh --clobber checkout tyler')
         }
     }
 }
