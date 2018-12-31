25e3cdae4fc92e361a7533ac3945557096361523
Tyler Young <tyler@x-plane.com>
2018-12-31 15:53:29 -0600

    fix (python virtual environment): Fix for missing notify method

--- vars/setUpPython3VirtualEnvironment.groovy
+++ vars/setUpPython3VirtualEnvironment.groovy
@@ -4,7 +4,7 @@ def call(utils, String platform='') {
         utils.chooseShell('virtualenv env -p python3', platform)
         utils.chooseShell("env/${binDir}/pip install -r package_requirements.txt", platform)
     } catch(e) {
-        notifyBuild("Virtual environment setup failed", "Check the logs.", e.toString(), "tyler@x-plane.com")
+        utils.notify("Virtual environment setup failed", "Check the logs.", e.toString(), "tyler@x-plane.com")
         throw e
     }
 }
