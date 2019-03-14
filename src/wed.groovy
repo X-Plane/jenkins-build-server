56e451470f70084d222fb914b85f237a4ceb21bd
Tyler Young <tyler@x-plane.com>
2019-03-14 14:50:03 -0500

    fix (WED): Entirely new approach to upload files :*(

--- src/wed.groovy
+++ src/wed.groovy
@@ -15,6 +15,7 @@ utils.setEnvironment(environment, this.&notify, this.steps)
 try {
     stage('Checkout') { runOn3Platforms(this.&doCheckout) }
     stage('Build & Archive') { runOn3Platforms(this.&doBuildAndArchive)    }
+    stage('Finalize Upload') { node('linux') { finalizeUpload('Linux') }   }
 } finally {
     node('windows') { step([$class: 'LogParserPublisher', failBuildOnError: false, parsingRulesPath: 'C:/jenkins/log-parser-builds.txt', useProjectRule: false]) }
 }
@@ -29,7 +30,8 @@ def runOn3Platforms(Closure c) {
 }
 
 def doCheckout(String platform) {
-    clean(getExpectedWedProducts(platform) + ['*.zip', '*.WorldEditor', getPublishableZipName(platform) + '*'], [], platform, utils)
+    clean(getExpectedWedProducts(platform) + ['*.zip', '*.WorldEditor'], [], platform, utils)
+    fileOperations([folderDeleteOperation(getPublishableZipName(platform))])
     try {
         xplaneCheckout(branch_name, utils.getCheckoutDir(platform), platform, 'https://github.com/X-Plane/xptools.git')
     } catch(e) {
@@ -59,15 +61,27 @@ def doBuildAndArchive(String platform) {
             notifyDeadBuild(utils.&sendEmail, 'WED', branch_name, utils.getCommitId(platform), platform, e)
         }
 
-        List<String> expectedProducts = getExpectedWedProducts(platform);
-        List<String> productPaths = utils.addPrefix(expectedProducts, utils.chooseByPlatformMacWinLin(['', 'msvc\\WorldEditor\\Release\\', 'build/Linux/release_opt/'], platform))
+
+        String exe = getWedExe(platform)
+        String exePath = utils.addPrefix([exe], utils.chooseByPlatformMacWinLin(['', 'msvc\\WorldEditor\\Release\\', 'build/Linux/release_opt/'], platform))[0]
+        String readme = 'README.WorldEditor'
+        String targetZipName = getPublishableZipName(platform)
+        String targetZip = "${targetZipName}.zip"
 
         try {
             // If we're on macOS, the "executable" is actually a directory within an xcarchive directory.. we need to ZIP it, then operate on the ZIP files
             if(utils.isMac(platform)) {
-                zip(zipFile: 'WED.app.zip', archive: false, dir: 'WED.xcarchive/Products/Applications/')
+                sh "zip -qr ${targetZip} WED.xcarchive/Products/Applications/* src/WEDCore/${readme}"
+            } else if(utils.isLinux(platform)) {
+                sh "zip -qr ${targetZip} ${exePath} src/WEDCore/${readme}"
+            } else {
+                // Move the EXE to the root directory so that the final ZIP will be "flat"
+                utils.chooseShell("mkdir ${targetZipName}", platform)
+                utils.copyFilePatternToDest(productPaths.first(), "${targetZipName}/${exe}")
+                utils.copyFilePatternToDest("src/WEDCore/${readme}", "${targetZipName}/${readme}")
+                zip(zipFile: targetZip, archive: false, dir: targetZipName)
             }
-            archiveWithDropbox(productPaths, getArchiveDirAndEnsureItExists(platform, 'WED'), true, utils, false)
+            archiveWithDropbox(targetZip, getArchiveDirAndEnsureItExists(platform, 'WED'), true, utils, false)
         } catch (e) {
             utils.sendEmail("WED archive step failed on ${platform} [${branch_name}]",
                     "Archive step failed on ${platform}, branch ${branch_name}. This is probably due to missing the WED executable.",
@@ -76,33 +90,27 @@ def doBuildAndArchive(String platform) {
         }
 
         if(publish_as_version && publish_as_version.length() == 5) { // we want to copy it to the live server
-            String targetZipName = getPublishableZipName(platform)
-            String targetZip = "${targetZipName}.zip"
-
-            String readme = 'README.WorldEditor'
-
-            if(utils.isMac(platform)) {
-                utils.copyFilePatternToDest(productPaths.first(), targetZip)
-                sh "zip -j ${targetZip} src/WEDCore/${readme}"
-            } else {
-                // Move the EXE to the root directory so that the final ZIP will be "flat"
-                utils.chooseShell("mkdir ${targetZipName}", platform)
-                utils.copyFilePatternToDest(productPaths.first(), "${targetZipName}/${expectedProducts.first()}")
-                utils.copyFilePatternToDest("src/WEDCore/${readme}", "${targetZipName}/${readme}")
-                zip(zipFile: targetZip, archive: false, dir: targetZipName)
-            }
             sshPublisher(publishers: [
                     sshPublisherDesc(
                             configName: 'DevTools',
-                            transfers: [sshTransfer(sourceFiles: targetZip, execCommand: "chmod o+r ${targetZip}")],
+                            transfers: [sshTransfer(sourceFiles: targetZip)],
                     )
             ])
         }
     }
 }
 
-List<String> getExpectedWedProducts(String platform) {
-    return [utils.chooseByPlatformMacWinLin(['WED.app.zip', 'WorldEditor.exe', 'WED'], platform)]
+def finalizeUpload(String platform) {
+    if(publish_as_version && publish_as_version.length() == 5) { // we copied files to the live server
+        for(def p : ['macOS', 'Windows', 'Linux']) {
+            def zipName = getPublishableZipName(p)
+            sh "ssh dev.x-plane.com chmod o+r /shared/download/tools/${zipName}.zip"
+        }
+    }
+}
+
+String getWedExe(String platform) {
+    return utils.chooseByPlatformMacWinLin(['WED.app.zip', 'WorldEditor.exe', 'WED'], platform)
 }
 
 String getPublishableZipName(String platform) {
