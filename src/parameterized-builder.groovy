0d94fa0e4e59e96a75c0c050564fcc17a4b34cda
Tyler Young <tyler@x-plane.com>
2019-03-27 13:41:49 -0500

    text (Slack): Added platform name to failed build message

--- src/parameterized-builder.groovy
+++ src/parameterized-builder.groovy
@@ -211,7 +211,7 @@ def doBuild(String platform) {
             String parsedLogUrl = "${BUILD_URL}parsed_console/"
             slackSend(
                     color: 'danger',
-                    message: "${heyYourBuild} of `${branch_name}` failed | <${parsedLogUrl}|Parsed Console Log> | <${BUILD_URL}|Build Info>")
+                    message: "${heyYourBuild} of `${branch_name}` failed on ${platform} | <${parsedLogUrl}|Parsed Console Log> | <${BUILD_URL}|Build Info>")
             alerted_via_slack = true
             notifyDeadBuild(utils.&sendEmail, 'X-Plane', branch_name, utils.getCommitId(platform), platform, e)
         }
