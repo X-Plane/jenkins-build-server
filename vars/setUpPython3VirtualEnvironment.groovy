b4790581ec367dbe917abe8b095c955849e0eac0
Tyler Young <tyler@x-plane.com>
2019-04-25 13:59:28 -0500

    fix (python venv): Fix for string interp in virtual environment setup

--- vars/setUpPython3VirtualEnvironment.groovy
+++ vars/setUpPython3VirtualEnvironment.groovy
@@ -3,7 +3,7 @@ def call(utils, String platform='') {
     String python3Path = utils.chooseByPlatformNixWin('python3', '"C:\\Program Files\\Python37\\python.exe"', platform)
     String binDir = utils.chooseByPlatformNixWin('bin', 'Scripts', platform)
     try {
-        utils.chooseShell('virtualenv env -p ${python3Path}', platform)
+        utils.chooseShell("virtualenv env -p ${python3Path}", platform)
         utils.chooseShell("env${dirChar}${binDir}${dirChar}pip install -r package_requirements.txt", platform)
     } catch(e) {
         utils.notify("Virtual environment setup failed", "Check the logs.", e.toString(), "tyler@x-plane.com")
