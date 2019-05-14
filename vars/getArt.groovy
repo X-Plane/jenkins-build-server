e80e5b77e11ba9accef7e12734776436ad25fd9a
Tyler Young <tyler@x-plane.com>
2019-05-14 08:41:23 -0500

    fix (getArt): Added retries to handle temporary connectivity issues when doing SVN checkouts

--- vars/getArt.groovy
+++ vars/getArt.groovy
@@ -2,10 +2,15 @@ def call(String checkoutDir) {
     dir(checkoutDir) {
         sshagent(['tylers-ssh']) {
             echo "Pulling SVN art assets too for later auto-testing"
-            // Do recursive cleanup, just in case
-            sh(returnStdout: true, script: "set +x find Aircraft  -type d -exec \"svn cleanup\" \\;")
-            sh(returnStdout: true, script: "set +x find Resources -type d -exec \"svn cleanup\" \\;")
-            sh(returnStdout: true, script: 'scripts/get_art.sh --clobber checkout tyler')
+            for(int i = 0; i < 5; ++i) { // retry a few times to account for connection issues
+                try {
+                    // Do recursive cleanup, just in case
+                    sh(returnStdout: true, script: "set +x find Aircraft  -type d -exec \"svn cleanup\" \\;")
+                    sh(returnStdout: true, script: "set +x find Resources -type d -exec \"svn cleanup\" \\;")
+                    sh(returnStdout: true, script: 'scripts/get_art.sh --clobber checkout tyler')
+                    return
+                } catch(e) { sleep(10) }
+            }
         }
     }
 }
