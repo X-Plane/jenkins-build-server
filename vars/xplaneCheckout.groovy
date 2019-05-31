ee15a8c2ed6c522eca0976a890b6be8215b82aec
Tyler Young <tyler@x-plane.com>
2019-05-31 14:54:16 -0500

    fix (checkout): No, seriously, nuke all the leftovers from the GloMo submodules

--- vars/xplaneCheckout.groovy
+++ vars/xplaneCheckout.groovy
@@ -13,7 +13,8 @@ def call(String branchName='', String checkoutDir='', String platform='', String
     dir(checkoutDir) {
         echo "Checking out ${branchName} on ${platform}"
 
-        for(String toClean : ["Resources/default scenery/default apt dat/", "Custom Scenery/Global Airports/"]) {
+        sh(returnStatus: true, script: 'git rm -r --cached Resources/mobile data/')
+        for(String toClean : ["Resources/default scenery/default apt dat/", "Custom Scenery/Global Airports/", 'Resources/mobile data/CIFP/']) {
             if(utils.shellIsSh(platform)) {
                 sh(returnStatus: true, script: "rm -rf \"${toClean}\"")
             } else {
@@ -25,7 +26,9 @@ def call(String branchName='', String checkoutDir='', String platform='', String
             sshagent(['tylers-ssh']) {
                 sh "git branch"
                 sh(returnStdout: true, script: "git fetch --tags")
-                sh(returnStdout: true, script: "git reset --hard")
+                try {
+                    sh(returnStdout: true, script: "git reset --hard")
+                } catch(e) { }
                 sh "git checkout ${branchName}"
                 sh(returnStatus: true, script: "git pull") // If we're in detached HEAD mode, pull will fail
             }
@@ -37,7 +40,6 @@ def call(String branchName='', String checkoutDir='', String platform='', String
         }
 
         if(utils.shellIsSh(platform)) {
-            sh(returnStatus: true, script: 'git rm --cached Resources/mobile data/CIFP/')
             dir(checkoutDir + 'scripts') {
                 if(fileExists('setup_submodules.sh')) {
                     sshagent(['tylers-ssh']) {
