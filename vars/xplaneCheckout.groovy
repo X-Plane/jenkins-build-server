a175188a46893e470f053499767d073f4481b868
Tyler Young <tyler@x-plane.com>
2019-06-05 06:46:16 -0500

    fix (checkout): Only recreate the submodule setup on Windows if we're checking out X-Plane

--- vars/xplaneCheckout.groovy
+++ vars/xplaneCheckout.groovy
@@ -36,48 +36,48 @@ def call(String branchName='', String checkoutDir='', String platform='', String
             )
         }
 
-        if(utils.shellIsSh(platform)) {
-            dir(checkoutDir + 'scripts') {
-                if(fileExists('setup_submodules.sh')) {
+        if(fileExists('scripts/setup_submodules.sh')) {
+            if(utils.shellIsSh(platform)) {
+                dir(checkoutDir + 'scripts') {
                     sshagent(['tylers-ssh']) {
                         sh './setup_submodules.sh'
                     }
                 }
-            }
-        } else { // Gotta recreate the damn setup_submodules.sh script on Windows
-            String remote = bat(returnStdout: true, script: "git remote get-url --push origin").trim().split("\r?\n")[1]
-            String remoteParent = remote.substring(0, remote.lastIndexOf('/') + 1)
-            bat "git config --file=.gitmodules \"submodule.Resources\\dlls\\64\\cef.url\" ${remoteParent}cef.git"
-            bat "git config --file=.gitmodules \"submodule.Resources\\default scenery\\default atc.url\" ${remoteParent}atc_res.git"
-            bat "git config --file=.gitmodules \"submodule.Resources\\default scenery\\default apt dat.url\" ${remoteParent}default_apts.git"
-            bat "git config --file=.gitmodules \"submodule.Custom Scenery\\Global Airports.url\" ${remoteParent}global_apts.git"
-            // Now do the checkout again, with the proper submodules
-            try {
-                checkout(
-                        [$class: 'GitSCM', branches: [[name: branchName]],
-                         doGenerateSubmoduleConfigurations: false,
-                         extensions: [
-                                 [$class: 'CloneOption', timeout: 120],
-                                 [$class: 'SubmoduleOption',
-                                       disableSubmodules: false,
-                                       parentCredentials: true,
-                                       recursiveSubmodules: true]
-                         ], submoduleCfg: [],
-                         userRemoteConfigs:  [[credentialsId: 'tylers-ssh', url: repo]]]
-                )
-            } catch(e) {
-                checkout(
-                        [$class: 'GitSCM', branches: [[name: branchName]],
-                         doGenerateSubmoduleConfigurations: false,
-                         extensions: [
-                                 [$class: 'CloneOption', timeout: 120],
-                                 [$class: 'SubmoduleOption',
-                                       disableSubmodules: false,
-                                       parentCredentials: true,
-                                       recursiveSubmodules: true]
-                         ], submoduleCfg: [],
-                         userRemoteConfigs:  [[credentialsId: 'tylers-ssh', url: repo]]]
-                )
+            } else { // Gotta recreate the damn setup_submodules.sh script on Windows
+                String remote = bat(returnStdout: true, script: "git remote get-url --push origin").trim().split("\r?\n")[1]
+                String remoteParent = remote.substring(0, remote.lastIndexOf('/') + 1)
+                bat "git config --file=.gitmodules \"submodule.Resources\\dlls\\64\\cef.url\" ${remoteParent}cef.git"
+                bat "git config --file=.gitmodules \"submodule.Resources\\default scenery\\default atc.url\" ${remoteParent}atc_res.git"
+                bat "git config --file=.gitmodules \"submodule.Resources\\default scenery\\default apt dat.url\" ${remoteParent}default_apts.git"
+                bat "git config --file=.gitmodules \"submodule.Custom Scenery\\Global Airports.url\" ${remoteParent}global_apts.git"
+                // Now do the checkout again, with the proper submodules
+                try {
+                    checkout(
+                            [$class: 'GitSCM', branches: [[name: branchName]],
+                             doGenerateSubmoduleConfigurations: false,
+                             extensions: [
+                                     [$class: 'CloneOption', timeout: 120],
+                                     [$class: 'SubmoduleOption',
+                                           disableSubmodules: false,
+                                           parentCredentials: true,
+                                           recursiveSubmodules: true]
+                             ], submoduleCfg: [],
+                             userRemoteConfigs:  [[credentialsId: 'tylers-ssh', url: repo]]]
+                    )
+                } catch(e) {
+                    checkout(
+                            [$class: 'GitSCM', branches: [[name: branchName]],
+                             doGenerateSubmoduleConfigurations: false,
+                             extensions: [
+                                     [$class: 'CloneOption', timeout: 120],
+                                     [$class: 'SubmoduleOption',
+                                           disableSubmodules: false,
+                                           parentCredentials: true,
+                                           recursiveSubmodules: true]
+                             ], submoduleCfg: [],
+                             userRemoteConfigs:  [[credentialsId: 'tylers-ssh', url: repo]]]
+                    )
+                }
             }
         }
 
